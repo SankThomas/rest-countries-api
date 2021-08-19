4006cd8eb7f7b37ff6874e84c8e0dfa6df59db86
Thomas Sankara <thomassankara74@gmail.com>
2021-08-19 18:47:31 +0300

    Filter component + conditional rendering for filtered items

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Filter from './Filter'
 
 const url = 'https://restcountries.eu/rest/v2/all'
 
 const Countries = () => {
   const [countries, setCountries] = useState([])
+  const [filtered, setFiltered] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchCountries = async () => {
@@ -25,8 +29,10 @@ const Countries = () => {
 
   return (
     <>
-      <section className="countries">
-        {countries.map((country) => {
+
+    <Filter searchInput={searchInput} setSearchInput={setSearchInput} setFiltered={setFiltered} countries={countries} />
+      {searchInput.length > 1 ? <section className="countries">
+        {filtered.map((country) => {
           const {
             numericCode,
             name,
@@ -69,7 +75,52 @@ const Countries = () => {
             </article>
           )
         })}
-      </section>
+      </section>:
+      <section className="countries">
+      {countries.map((country) => {
+        const {
+          numericCode,
+          name,
+          flag,
+          population,
+          region,
+          capital,
+        } = country
+
+        return (
+          <article key={numericCode}>
+            <div className="flag">
+              <img src={flag} alt={name} />
+            </div>
+            <div className="details">
+              <h4 className="country-name">
+                Name: <span>{name}</span>
+              </h4>
+              <h4>
+                Population: <span>{population}</span>
+              </h4>
+              <h4>
+                Region: <span>{region}</span>
+              </h4>
+              <h4>
+                Capital: <span>{capital}</span>
+              </h4>
+              <div className="buttons">
+                <Link to={`/countries/${name}`} className="btn">
+                  Learn More
+                </Link>
+                <button
+                  className="btn"
+                  onClick={() => removeCountry(numericCode)}
+                >
+                  Remove Country
+                </button>
+              </div>
+            </div>
+          </article>
+        )
+      })}
+    </section>}
     </>
   )
 }
