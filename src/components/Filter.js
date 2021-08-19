19b07d064f26530096b91d47b7c1b617cf8e9f3f
Thomas Sankara <thomassankara74@gmail.com>
2021-08-19 18:47:08 +0300

    add search filtering

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,32 +1,51 @@
 import React from 'react'
 
-const Filter = () => {
-  window.addEventListener('DOMContentLoaded', () => {
-    const search = document.getElementById('search')
+const Filter = ({searchInput, setSearchInput, setFiltered, countries}) => {
+  // window.addEventListener('DOMContentLoaded', () => {
+  //   const search = document.getElementById('search')
 
-    search.addEventListener('click', (e) => {
-      const { value } = e.target
+  //   search.addEventListener('click', (e) => {
+  //     const { value } = e.target
 
-      const countryName = document.querySelectorAll('.country-name')
+  //     const countryName = document.querySelectorAll('.country-name')
 
-      countryName.forEach((name) => {
-        if (name.innerText.toLowerCase().includes(value.toLowerCase())) {
-          name.parentElement.parentElement.style.display = 'block'
-        } else {
-          name.parentElement.parentElement.style.display = 'none'
-        }
-      })
-    })
-  })
+  //     countryName.forEach((name) => {
+  //       if (name.innerText.toLowerCase().includes(value.toLowerCase())) {
+  //         name.parentElement.parentElement.style.display = 'block'
+  //       } else {
+  //         name.parentElement.parentElement.style.display = 'none'
+  //       }
+  //     })
+  //   })
+  // })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const searchCountries = (searchValue) => {
+    setSearchInput(searchValue)
+
+    if (searchInput) {
+      const filteredCountries = countries.filter((country) => (
+        Object.values(country).join("").toLowerCase().includes(searchValue.toLowerCase())
+      ))
+      setFiltered(filteredCountries)
+    } else {
+      setFiltered(countries)
+    }
+  }
 
   return (
     <>
-      <form className="form" id="form">
+      <form className="form" id="form" onSubmit={handleSubmit}>
         <input
           type="search"
           name="search"
           id="search"
+          autoComplete="off"
           placeholder="Search Country"
+          onChange={(e) => searchCountries(e.target.value)}
         />
 
         <div className="select">
